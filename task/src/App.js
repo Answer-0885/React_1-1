c9ef7e8776d1ed88985d0ff39cdd3537aa971f1c
Vitaly Zakatov <answer-0885@mail.ru>
2023-01-20 14:34:04 +0700

    Задание 1-1

---
+++ task/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import ShopItemFunc from './components/ShopItemFunc';
+
+function App(item) {
+  return (
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemFunc item={item} />
+      </div>
+    </div>
+  )
+}
+
+export default App;
